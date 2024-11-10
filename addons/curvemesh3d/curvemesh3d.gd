309d0148f0f0a486ec24292a717936bc071c8af4
cloudofoz <120678869+cloudofoz@users.noreply.github.com>
2024-11-10 07:43:43 +0100

    add class_name CurveMesh3D for easier integration in scripts

--- addons/curvemesh3d/curvemesh3d.gd
+++ addons/curvemesh3d/curvemesh3d.gd
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 Claudio Z. (cloudofoz)
+# Copyright (C) 2022-2024 Claudio Z. (cloudofoz)
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,8 @@
 
 @tool
 extends Path3D
+
+class_name CurveMesh3D
 
 #---------------------------------------------------------------------------------------------------
 # CONSTANTS
